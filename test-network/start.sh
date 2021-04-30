1df212c56d443a6c9e87a5c0691ca3aa78bf5e0d
Eric Fan <ericfan@cs.washington.edu>
2021-04-30 06:11:38 +0000

    Upload start.sh

---
+++ test-network/start.sh
@@ -0,0 +1,4 @@
+rm -rf ../application-javascript/wallet/
+./network.sh down
+./network.sh up createChannel -c mychannel -ca
+./network.sh deployCC -ccn basic -ccp ../chaincode-javascript/ -ccl javascript
